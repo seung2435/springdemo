640bba90aba1674181b004673fdbe7341d5e68ad
seung2435 <seung2435@naver.com>
2022-06-04 11:24:05 +0900

    2022.06.04. 실습

--- src/main/java/com/acompany/springdemo/controller/HomeController.java
+++ src/main/java/com/acompany/springdemo/controller/HomeController.java
@@ -1,4 +1,23 @@
 package com.acompany.springdemo.controller;
 
+import com.acompany.springdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
 public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("")
+    public Map<String, String> Home() {
+        Map<String, String> res = this.userService.getMessage();
+        return res;
+    }
 }
---
+++ src/main/java/com/acompany/springdemo/service/UserService.java
@@ -0,0 +1,15 @@
+package com.acompany.springdemo.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserService {
+    public Map<String, String> getMessage() {
+        Map<String, String> res = new HashMap<>();
+        res.put("greet", "Hello World");
+        return res;
+    }
+}
